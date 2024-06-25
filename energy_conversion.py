baa42a75ab4036de834993348d2c8308dbf05e18
v4vincent <vermillionvincent@gmail.com>
2024-06-24 21:06:40 -0500

    testing floats

---
+++ energy_conversion.py
@@ -0,0 +1,7 @@
+c_meters_per_sec = 299792458
+
+mass_kg = float(input("Enter the mass kg: "))
+
+energy_per_joules = mass_kg * (c_meters_per_sec ** 2)
+
+print("Energy per joules: ", energy_per_joules)
--- float.py
+++ float.py
@@ -2,3 +2,8 @@
 float_num = float(input("Tell me a float: "))
 
 print(float_num)
+
+x = 15
+y = 5
+y = 5.4e8
+print(y)
