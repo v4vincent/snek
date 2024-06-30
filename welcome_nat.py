1198d3a807f7e02685c47945de087756d407f4e6
v4vincent <vermillionvincent@gmail.com>
2024-06-29 19:36:51 -0500

    true welcome for the princess

--- welcome_nat.py
+++ welcome_nat.py
@@ -1,10 +1,1 @@
-<<<<<<< HEAD
-<<<<<<< HEAD
-print("Hello, Natalie Rosemarie")
-=======
-print("Happy Baby Shower, Nathalie!")
->>>>>>> e66dc8f (from vs code)
-=======
-print("Happy Baby Shower, Nathalie!")
-print("it's been a great day")
->>>>>>> 13360d5 (vs code)
+print("Welcome to the world, baby Natalie Rosemarie! Daddy loves you very much.")
