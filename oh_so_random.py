e818bc26b60b39c46ec996861d09db64e830c2ca
v4vincent <vermillionvincent@gmail.com>
2024-06-29 21:37:56 -0500

    seeded in randomness

--- oh_so_random.py
+++ oh_so_random.py
@@ -1,4 +1,10 @@
 
 import random
 
-print(f"random number: {random.random():.2f}")
+# random.seed(5)
+
+print(f"random number: {random.random() * 100 :.0f}")
+
+print(random.randrange(5))
+
+print(random.randrange(-10,10))
