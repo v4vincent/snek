67b37deed050f9af5deae91b693102461d895a85
v4vincent <vermillionvincent@gmail.com>
2024-06-29 19:22:01 -0500

    vs code

--- welcome_nat.py
+++ welcome_nat.py
@@ -1,5 +1,10 @@
 <<<<<<< HEAD
+<<<<<<< HEAD
 print("Hello, Natalie Rosemarie")
 =======
 print("Happy Baby Shower, Nathalie!")
 >>>>>>> e66dc8f (from vs code)
+=======
+print("Happy Baby Shower, Nathalie!")
+print("it's been a great day")
+>>>>>>> 13360d5 (vs code)
