974aa84e3509cbab3bd7ba0f142be7a43db454f3
v4vincent <vermillionvincent@gmail.com>
2024-08-19 22:18:08 -0700

    print f stuff

---
+++ simple_labs.py
@@ -0,0 +1,9 @@
+mpg = float(input())
+cost_of_gas = float(input())
+
+your_value1 = (20 / mpg) * cost_of_gas
+your_value2 = (75 / mpg) * cost_of_gas
+your_value3 = (500 / mpg) * cost_of_gas
+
+print(f'{your_value1:.2f} {your_value2:.2f} {your_value3:.2f}')
+
