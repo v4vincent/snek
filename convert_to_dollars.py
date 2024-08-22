97203123ce1d64c1d21a3a1c2ac9f4f4647d8594
v4vincent <vermillionvincent@gmail.com>
2024-08-21 21:59:42 -0700

    added balance

--- convert_to_dollars.py
+++ convert_to_dollars.py
@@ -6,4 +6,4 @@ pennies = int(input())
 
 final_value = (quarters * 25 / 100) + (dimes * 10 / 100) + (nickles * 5 / 100) + (pennies / 100)
 
-print(f'Amount: ${final_value:.2f}')
+print(f'Balance Amount: ${final_value:.2f}')
