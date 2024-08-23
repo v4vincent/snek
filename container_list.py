f3ecab8bbb2bfdf47bde0e2eed7e0ea62083df80
v4vincent <vermillionvincent@gmail.com>
2024-08-23 05:59:07 -0700

    playing with lists again

--- container_list.py
+++ container_list.py
@@ -1,9 +1,12 @@
 
 class FirstObject:
-    x = 5
+   print("This is from the FirstObject object")
 
 object1 = FirstObject()
 
 list_a = ['abc', 123, False, 1.5, 'a', object1]
 
 print(list_a)
+
+for i in list_a:
+   print(i)
