3a9dbf12a2a35feca24dc7b49d9a1c753cf75fe4
v4vincent <vermillionvincent@gmail.com>
2024-08-24 15:47:28 -0700

    learning more about lists

--- container_list.py
+++ container_list.py
@@ -10,3 +10,17 @@ print(list_a)
 
 for i in list_a:
    print(i)
+
+list_a.append("Add")
+print(list_a)
+
+list_a.pop(5)
+
+list_b = [1,2,3,4]
+
+combined_list = list_a + list_b
+
+print(combined_list.index(False))
+
+
+
