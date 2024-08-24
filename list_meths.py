f00951ab862d066e576d5ed55a42227e39e70853
v4vincent <vermillionvincent@gmail.com>
2024-08-24 16:02:41 -0700

    list methods

---
+++ list_meths.py
@@ -0,0 +1,3 @@
+
+house_prices = [150_000, 160_000,200_000,1_000_000]
+print(f'There are {len(house_prices)} houses available in the market. The least is ${min(house_prices)} and the most expensive is ${max(house_prices)}.')
