5225ec07f8c20095063e58e36423a8d6d06bd5b9
v4vincent <vermillionvincent@gmail.com>
2024-11-11 15:41:14 -0800

    learning sets

---
+++ dicts.py
@@ -0,0 +1,20 @@
+family = {"Mother": "Nicole Vermillion",
+          "Father": "Vincent Vermillion",
+          "Daughter": "Natalie Vermillion"
+          }
+
+
+for i in family:
+    print(i)
+
+
+saints = {}
+
+saints[1] = "John"
+saints[2] = "Peter"
+
+print(saints)
+
+del saints[2]
+
+print(saints)
