6240faca79d71b4d92387386033f7f5e04ae378d
v4vincent <vermillionvincent@gmail.com>
2024-11-13 12:34:42 -0800

    learning string formats

--- f_strings.py
+++ f_strings.py
@@ -11,3 +11,13 @@ def string_manipulation(p1,p2,p3):
     print(f"{p3} is their daughter.")
 
 string_manipulation(wife,husband,daughter)
+
+def f_strings(string):
+    if isinstance(string, str):
+        print(f"{string:s}")
+    elif isinstance(string, int):
+        print(f"{string:.2f}")
+
+
+f_strings(1)
+f_strings("Vincent")
