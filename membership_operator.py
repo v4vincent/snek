6284a3abd55c0d8ae1c47b6320faa52e730166e6
v4vincent <vermillionvincent@gmail.com>
2024-11-19 21:59:04 -0800

    one liner solution

--- membership_operator.py
+++ membership_operator.py
@@ -4,3 +4,13 @@ if ('N' and 'a' in my_string) and 'B' not in my_string:
     print("True")
 else:
     print("False")
+
+family_list = ["Vincent", "Nicole", "Natalie"]
+family_list_cap = [i.capitalize() for i in family_list]
+
+name = input("What's your name? ").capitalize()
+
+if name in family_list_cap:
+    print(f"Yep, {name}, you're a Vermillion")
+else:
+    print(f'{name}? Never heard of you.')
